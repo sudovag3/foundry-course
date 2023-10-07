8bbce4ac0c4aceb1d4f9b91afe98e004ef649b10
daandrey <sudovae03@mail.ru>
2023-10-07 08:35:16 +0300

    start refactoring

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,57 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+contract Counter {
+    uint256 testVar;
 
-//НЕ ИСПОЛЬЗОВАТЬ ДЛЯ РЕЛИЗА
-//Импортируем console для вывода логов прямо из контракта
-import {console} from "forge-std/Test.sol";
-
-contract Counter is Ownable {
-    uint256 public number;
-    uint256 public lastTransferTime;
-
-    event Incremented(uint256 indexed number);
-
-    error ZeroNumber();
-
-    constructor() Ownable() {
-        console.log("It's Counter contract, my owner is ", owner());
-    }
+    constructor() {}
 
     /**
-     * Функция для установления нового значения number
-     * Доступна для вызова только owner'y
-     * @param newNumber не должен быть равен 0
+     * Данная функция возвращает различные значения в
+     * зависимости от значений аргумента
+     * По названию можно чётко понять, что по бизнес-логике
+     * наша функция не должна возвращать ноль
+     *
+     * Здесь довольно очевидно (в целях обучения)
+     * спрятан баг, который нарушает бизнес-логику
+     * этой функции.
+     *
+     * При помощи state-less fuzz-тестирования мы скорее всего
+     * очень легко найдём этот баг
+     *
+     * @param x Обрабатываемое число
      */
-    function setNumber(uint256 newNumber) public onlyOwner {
-        if (newNumber == 0) {
-            console.log("This function is going to fail!");
-            revert ZeroNumber();
+    function NeverReturnZero(uint256 x) public pure returns (uint256) {
+        if (x == 1) {
+            return 123 - x;
+        } else if (x % 10 == 0) {
+            //Здесь всегда будет возвращаться ноль
+            return (x % (78123 - 78113));
+        } else if (x % 123 == 1) {
+            return 321;
+        } else {
+            return 0;
         }
-        console.log("Setting number - ", newNumber);
-        number = newNumber;
     }
-
     /**
-     * Увеличивает значение number на 1
-     * Инициирует событие Incremented
+     * Данная функцию уже не получится протестировать с помощью
+     * state-less fuzz тестирования
+     *
+     * В данном случае баг вызовется только в сценарии,
+     * когда состояние предыдущей итерации будет влиять на
+     * состояние следующей
+     *
+     * Здесь на помощь может прийти только stateful
+     * fuzz-тестирование
+     *
+     * @param x Обрабатываемое число
      */
-    function increment() public {
-        console.log("Incrementing number - ", number);
-        number++;
-        emit Incremented(number);
+
+    function NeverReturnZero2(uint256 x) public returns (uint256) {
+        //
+        if (x < 10) {
+            revert();
+        }
+
+        if (x % 5 == 0) {
+            testVar += 100;
+        }
+
+        if (testVar == 200) {
+            //Может вызваться, если до этого два раза
+            //вызвать функцию с аргументом, кратным 5
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
-    /**
-     * Стандартный метод на принятие нативной валюты, выводим в лог информацию об отправителе
-     * и о количестве отправленного эфира
-     */
-    receive() external payable {
-        //Ставим ограничение по сроку пополнения баланса контракта
-        require((block.timestamp >= lastTransferTime + 1 days) || lastTransferTime == 0, "Try later!");
-        lastTransferTime = block.timestamp;
+    function increment() public {}
 
-        console.log("Transfer  ", msg.value, " from: ", msg.sender);
-    }
+    receive() external payable {}
 }
